12d4d6cab5b23d7d7facef2c5b1b6da69d349eb6
Eduardo Acosta <aczanied@gmail.com>
2021-12-21 17:31:24 -0500

    auditcats cammbios

--- contact_process.php
+++ contact_process.php
@@ -4,7 +4,7 @@
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
+    // $number = $_REQUEST['number'];
     $cmessage = $_REQUEST['message'];
 
     $headers = "From: $from";
@@ -13,9 +13,9 @@
 	$headers .= "MIME-Version: 1.0\r\n";
 	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 
-    $subject = "You have a message from your ConsultPlus.";
+    // $subject = "Tienes un nuevo Mensaje desde Auditcaats";
 
-    $logo = 'http://wethemez.com/test-html/consultplus/img/logo-black.png';
+    // $logo = 'https://auditcaats.divuss.com/img/auditcaats/blanco.png';
     $link = '#';
 
 	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
@@ -23,15 +23,17 @@
 	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
 	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
 	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
+	$body .= "<td style='border:none;'><strong>Nombre:</strong> {$name}</td>";
+	$body .= "<td style='border:none;'><strong>Correo:</strong> {$from}</td>";
 	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
+	$body .= "<tr><td style='border:none;'><strong>Asunto:</strong> {$subject}</td></tr>";
 	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
+	$body .= "<tr><td colspan='2' style='border:none;'><strong>Mensaje:</strong>{$cmessage}</td></tr>";
 	$body .= "</tbody></table>";
 	$body .= "</body></html>";
 
     $send = mail($to, $subject, $body, $headers);
+
+	header("Location:contacto.html");
 
 ?>
